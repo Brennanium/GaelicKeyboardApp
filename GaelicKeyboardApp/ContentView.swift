1187b7b1c49d9f5c0dc783cec046fa559acf34da
drewb <drewb@wwu.edu>
2020-03-25 15:25:25 -0700

    Initial Commit

---
+++ GaelicKeyboardApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GaelicKeyboardApp
+//
+//  Created by Brennanium on 3/25/20.
+//  Copyright © 2020 Brennanium. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
